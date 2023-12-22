f39c22c586b31380c2105bad6ce74dcd91c80b84
Jonas Lahme <jonas.lahme@joneva.de>
2023-12-22 14:48:33 +0100

    Inital Commit

---
+++ version.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Farzin K
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version  = 2023111300;
+$plugin->requires = 2019111200;
+$plugin->component = 'local_course_explorer_service';
