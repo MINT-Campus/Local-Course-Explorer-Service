77a77a808aaf28641aa9ce13d5e153928cb2823a
Thomas Winkler <thomas.winkler@wunderbyte.at>
2024-02-19 21:09:35 +0100

    version bump

--- version.php
+++ version.php
@@ -5,6 +5,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2023111300;
+$plugin->version  = 2024021900;
 $plugin->requires = 2019111200;
 $plugin->component = 'local_course_explorer_service';
